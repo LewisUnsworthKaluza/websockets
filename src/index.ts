705dd723bfd38f0e8b173637d04ceb763639ce17
Lewis <lewis.unsworth@kaluza.com>
2024-10-08 15:56:37 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { WebSocketServer } from 'ws';
+// @ts-ignore
+import data from './data2.json' assert { type: "json" };;
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send(JSON.stringify(data));
+});
